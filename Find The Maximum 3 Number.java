8313d1b9613782f4fd5c5e2774b5fb4452195b5e
BHAVIK DABHI <bhavikd113@gmail.com>
2025-05-09 21:32:58 +0530

    Update and rename README.md to Find The Maximum 3 Number.java

---
+++ Find The Maximum 3 Number.java
@@ -0,0 +1,19 @@
+FIND THE MAXIMUM 3 NUMBER:-
+
+public class NewClass1 {
+  public static void main(String[] args){
+    int a = 10;
+    int b = 39; 
+    int c = 49;
+    
+    if(a>b && a>c){
+      System.out.println("A is greater");
+      
+    }else if(b>c){
+      System.out.println("B is greater");
+    }else{
+      System.out.println("C is greater");
+    }
+      
+  }
+}
