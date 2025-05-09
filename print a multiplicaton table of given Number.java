04c95bd8a8d4e4e68dd7efdac66f285826100294
BHAVIK DABHI <bhavikd113@gmail.com>
2025-05-09 21:53:29 +0530

    Create  print a multiplicaton table of given Number.java

---
+++ print a multiplicaton table of given Number.java
@@ -0,0 +1,8 @@
+public class NewClass1 {
+  public static void main (String[] args){
+  int num = 215;
+  for(int i=1;i<=10;i++){
+    System.out.println(num + "*" + i + " = " + (num*i));
+    }
+  }
+}
