a07ba10ef5d25f8c1fa296680e9fd8e37ab0dcb1
BHAVIK DABHI <bhavikd113@gmail.com>
2025-05-09 21:37:34 +0530

    Create Check if the number is divisible by 3 & 5.java

---
+++ Check if the number is divisible by 3 & 5.java
@@ -0,0 +1,14 @@
+public class NewClass2 {
+  public static void main (String[] args) {
+    
+    // check if the number is divisible by 3 and 5
+    
+    int a = 15;
+    
+    if(a%3==0 && a%5==00){
+      System.out.println("Number is divisible by 3 and 5");
+    }else{
+      System.out.println("Not divisible");
+    }
+  }
+}
